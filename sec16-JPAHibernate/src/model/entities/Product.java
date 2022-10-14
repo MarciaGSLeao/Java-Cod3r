4494c3b78e726cc185335213946b824b48a2fdcd
Marcia Gameleira <gameleira.mag@hotmail.com>
2022-10-13 21:53:51 -0300

    .

--- sec16-JPAHibernate/src/model/entities/Product.java
+++ sec16-JPAHibernate/src/model/entities/Product.java
@@ -17,8 +17,7 @@ public class Product {
 	public Product() {
 	}
 
-	public Product(Long id, String name, Double price) {
-		this.id = id;
+	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
 	}
