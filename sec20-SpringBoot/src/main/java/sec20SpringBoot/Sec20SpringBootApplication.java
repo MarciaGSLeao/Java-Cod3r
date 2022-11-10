42256e3aaedbaaab6c3bb1cdc165d6093847c722
Marcia Gameleira <gameleira.mag@hotmail.com>
2022-11-10 02:55:06 -0300

    starting a maven project.

---
+++ sec20-SpringBoot/src/main/java/sec20SpringBoot/Sec20SpringBootApplication.java
@@ -0,0 +1,13 @@
+package sec20SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec20SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sec20SpringBootApplication.class, args);
+	}
+
+}
---
+++ sec20-SpringBoot/src/test/java/sec20SpringBoot/Sec20SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sec20SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec20SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
